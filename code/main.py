f8ef955b436932d86afbf560e21d7b50dd270cb2
alexkarvou <karvouniaris.alexander@gmail.com>
2021-10-30 15:47:37 +0300

    code for main.py

--- code/main.py
+++ code/main.py
@@ -1,2 +1,21 @@
+import tensorflow as tf
+from termcolor import colored
 
+
+# from env.enviroments import SimosFoodGroup
+from data.data_functions import  train_input_fn
+from utils.util_functions import create_meta_parameters
+from models.models import model_skeleton
+from training.training_functions import train
+
+
+
+# Metaparameters
+metaparameters = create_meta_parameters()
+
+# Data sources
+validation_dataset = eval_input_fn(metaparameters)
+
+# Train operation
+model = train(validation_dataset, metaparameters,config)
 
